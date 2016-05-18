5f18df64dd4267dde30354f4d0400034e502ac5e
christopher.zuber <christopher.zuber@MU-C1-17.mullen.pvt.k12.co.us>
2016-05-18 10:23:11 -0600

    Now it has current local weather for Denver and national weather for the US

--- Weather/src/Final.java
+++ Weather/src/Final.java
@@ -3,15 +3,16 @@ import org.jsoup.Jsoup;
 public class Final {
 	public static void main(String[] args) throws IOException 
 	{
-	String urltemp = "http://weather.gc.ca/city/pages/on-4_metric_e.html";
-	org.jsoup.nodes.Document document1 = Jsoup.connect(urltemp).get();
-	String temp = document1.select("p").get(1).text();
-	System.out.println("The current local temperature:");
-	System.out.println(temp);
-	String url = "http://www.weather.gov/forecastmaps";
-	org.jsoup.nodes.Document document = Jsoup.connect(url).get();
-	String weather = document.select("p").get(1).text();
-	System.out.println("The current national weather:");
-	System.out.println(weather);
+		String urltemp = "http://www.timeanddate.com/weather/usa/denver";
+		org.jsoup.nodes.Document document1 = Jsoup.connect(urltemp).get();
+		String temp = document1.select("p").get(1).text();
+		System.out.println("The current local weather:");
+		System.out.println(temp);
+		String url = "http://www.weather.gov/forecastmaps";
+		org.jsoup.nodes.Document document = Jsoup.connect(url).get();
+		String weather = document.select("p").get(1).text();
+		System.out.println("The current national weather:");
+		System.out.println(weather);
+		System.out.println("Heve a nice day!");
 	}
 }
